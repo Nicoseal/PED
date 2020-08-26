54835a298dc1312b022f212a46d9cb1801bc2622
Nicoseal <nicolasventieri@gmail.com>
2020-08-26 16:48:25 -0300

    Inicio del proyecto y agregado los routers

--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Main() {
     return(
         <div>
-            Hello World! v2
+
         </div>
     );
 }
--- src/router.js
+++ src/router.js
@@ -4,13 +4,19 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
  * sinó, si se está utilizando algo como Github Pages (Sitios Estáticos) usar HashRouter
  */
 
+import Principal from './pages/Principal'
+import PrincipalAlumno from './pages/Alumno/Principal';
+import PrincipalProfesor from './pages/Profesor/Principal';
+import PrincipalAdmin from './pages/Admin/Principal';
 import PaginaNoEncontrada from './components/error/404/PaginaNoEncontrada';
-import App from "./App";
 
 const Router = () => (
   <HashRouter>
       <Switch>
-        <Route exact path="/" component={App}/> 
+        <Route exact strict path="/" component={Principal} />
+        <Route exact strict path="/Alumno" component={PrincipalAlumno} />
+        <Route exact strict path="/Profesor" component={PrincipalProfesor} />
+        <Route exact strict path="/Admin" component={PrincipalAdmin} />
         <Route component={PaginaNoEncontrada} />
       </Switch>
   </HashRouter>
